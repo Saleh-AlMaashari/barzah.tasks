61f480683fb351dbc2463d3698bf4dc1f83d845f
Saleh Al Maashari <almaashari1996@gmail.com>
2025-07-13 15:09:39 +0400

    تحديث جديد

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/barzah.tasks/', // اسم الريبو تمامًا مع الـ "/"
+  base: '/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [react()],
 });
